50a04c79d39d3358de19cdd8e3f08d7064fec595
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-01-28 22:01:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,41 +1,38 @@
 class Solution {
-    Stack<Integer> st;
-    boolean[] vis;
+    int[] indegree;
+    int[] vis;
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        // code here
-        st = new Stack<>();
-        vis = new boolean[V];
+        ArrayList<Integer> ans = new ArrayList<>();
+        indegree = new int[V];
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
         for(int i = 0; i< V; i++){
             adj.add(new ArrayList<>());
         }
         for(int i = 0; i< edges.length; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            
-            adj.get(u).add(v);
+            adj.get(edges[i][0]).add(edges[i][1]);
+            indegree[edges[i][1]]++;
         }
-        
+        Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i< V; i++){
-            if(!vis[i]){
-                helper(i, adj);
+            if(indegree[i] == 0){
+                q.add(i);
             }
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        while(!st.isEmpty()){
-            ans.add(st.pop());
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            ans.add(curr);
+            
+            for(int i = 0; i< adj.get(curr).size(); i++){
+                int temp = adj.get(curr).get(i);
+                indegree[temp]--;
+                if(indegree[temp]== 0){
+                    q.add(temp);
+                }
+            }
         }
         return ans;
-    }
-    public void helper(int ind, ArrayList<ArrayList<Integer>> adj){
-        vis[ind] = true;
         
-        for(int i = 0; i<adj.get(ind).size(); i++){
-            if(!vis[adj.get(ind).get(i)]){
-                helper(adj.get(ind).get(i), adj);
-            }
-        }
-        st.push(ind);
         
     }
 }
